795a093f906ac8306ca0d6318c0cf56ed1b0f3f6
prabhjyot009 <prabh156dps@gmail.com>
2022-09-18 17:04:20 +0530

    html

---
+++ JAVA/College programs/A.java
@@ -0,0 +1,36 @@
+/*public class A
+{
+public static void main(String s[])
+}
+B x=new B();
+B y=new B();
+B.b=700;
+System.out.println(x.b);
+System.out.println(y.b);
+}
+class B
+{
+int a=10;//instance //object
+static int b=20;//either object or class
+}
+*/
+/*
+public class A
+{
+public static void main(String s[])
+{
+B b=new B();
+b.show();
+}}
+class B
+{
+int a;
+static int b;
+void show()
+{
+int c=10;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}  */
