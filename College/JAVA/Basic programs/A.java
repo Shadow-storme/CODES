7df1e2574682bc9c9382f3764ea5cb77071d2f29
prabhjyot009 <prabh156dps@gmail.com>
2022-10-15 20:40:53 +0530

    codes

--- College/JAVA/Basic programs/A.java
+++ College/JAVA/Basic programs/A.java
@@ -1,114 +1,73 @@
+//Object and classes
+//defining a student class
 /*class A
 {
-    public static void main(String s[])
+    int id;
+    String name;
+    public static void main(String[] args)
     {
-        A b=new A(100);
-        System.out.println(b.i);
+        A object =new A();
+        System.out.println(object.id);
+        System.out.println(object.name);
+    }
+}*/
+//Java Program to demonstrate having the main method in   
+//another class
+/*class A
+{
+    int id;
+    String name;
+}
+class testA
+{
+    public static void main(String[] args)
+    {
+        A object=new A();
+        System.out.println(object.id);
+        System.out.println(object.name);
+    }
+}*/
+//Initialization through reference
+/*
+// Overloading
+class overloading
+{
 
-        A b1=new A(100,200);
-        System.out.println(b1.i);
-
-        A b2
-    }
-}
-class A extends B
-{
-    A(int x)
-    {
-        super(x);
-    }
-    A(int x,int y)
-    {
-        super(x,y);
-    }
-}
-class B extends C
-{
-    int i;
-    B(int x)
-    {
-        i=x;
-    }
-    B(int x,int y)
-    {
-        i=x+y;
-    }
-}
-class C
-{
-    int 
+        static int add(int a,int b,int c)
+        {return(a+b+c);}
+        static int add(int a,int b)
+        {return(a+b);}    
 }
 class A
 {
     public static void main(String[] args)
     {
-        System.out.println("Screen display");
-        for(int i=0;i<=9;i++)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(" ");
-                System.out.print(i);
-            }
-            System.out.println("\n");
-        }
-        System.out.println("Screen Displaying done");
+        System.out.println(overloading.add(1, 5,7));
+        System.out.println(overloading.add (1,5));
     }
-}*/
-//Array*/
+}
+*/
+//Array initialization of a method
 /*class A
 {
     public static void main(String[] args)
-    {
-        int[] arr;
-        arr= new int[5];
-        arr[0]=10;
-        arr[1]=20;
-        arr[2]=30;
-        arr[3]=40;
-        arr[4]=50;
+{
+    int arr[][]=new int[3][];
+    arr[0]=new int[3];
+    arr[1]=new int[4];
+    arr[2]=new int[2];
 
-        for(int i=0;i<arr.length;i++)
-        System.out.println("Elements in index" +i+":"+arr[i]);
+    int count=0;
+
+    for(int i=0;i<arr.length;i++)
+    {
+    for(int j=0;j<arr[i].length;j++)
+    {arr[i][j]=count++;
+    }}
+    for(int i=0;i<arr.length;i++){
+          for(int j=0;j<arr[i].length;j++)
+          System.out.print(arr[i][j]+" ");  
+        }  
+        System.out.println();//new line  
     }
 }*/
-//Array of objects*/
-/*class Student {
-    public int roll_no;
-public String name;
-
-Student(int roll_no,String name)
-{
-    this.roll_no=roll_no;
-    this.name=name;    
-}
-}
-public class A
-{
-    public static void main(String[] args)
-    {
-        Student arr[];
-        arr= new Student[5];
-        arr[0]=new Student(1,"aman");
-        arr[1]=new Student(2,"naman");
-        arr[2]=new Student(3,"Jordan");
-        arr[3]=new Student(4,"sam");
-        arr[4]=new Student(5,"Khalid");
-
-        for(int i=0;i< arr.length;i++)
-        System.out.println("ELement at"+i+":"+arr[i].roll_no+" "+arr[i].name);
-    }
-}*/
-//2-D array*/
-class A
-{
-    public static void main(String[] args) 
-    {
-        int arr[]={3,1,2,5,4};
-        int 
-    }
-    public static void sum() 
-    {
-
-    }
-}
