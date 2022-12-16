031520e4f8aa27df8c7d0635c403f01f4adec0c2
prabhjyot009 <prabh156dps@gmail.com>
2022-12-16 06:15:16 +0530

    codes

--- College/JAVA/LeetCode/one.java
+++ College/JAVA/LeetCode/one.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 public class one {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array");
-         int length = sc.nextInt();
-         int[] nums = new int[length];
+        int length = sc.nextInt();
+        int[] nums = new int[length];
         for (int i = 0; i < length; i++) {
             nums[i] = sc.nextInt();
         }
@@ -16,6 +17,6 @@ public class one {
                 System.out.println("[" + i + "," + (i + 1) + "]");
             }
         }
-    
+
     }
 }
