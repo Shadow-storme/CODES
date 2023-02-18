1fd90b1413e9638646e4c8a7f23f8e4d32703e62
prabhjyot009 <prabh156dps@gmail.com>
2023-02-19 00:39:58 +0530

    commit

---
+++ College/Python/Course/Programs/String.py
@@ -0,0 +1,22 @@
+#String in python is immutable
+#String is an array
+#String is a sequence of characters
+#String is a collection of characters
+
+x="Hello"
+y="Sir"
+z=x+y
+print(z)
+
+#String Array
+print(x[1])
+print(y[0])
+
+#in reverse
+print(x[-1])
+print(y[-2])
+
+#String Length
+print(len(x))
+print(len(y))
+
